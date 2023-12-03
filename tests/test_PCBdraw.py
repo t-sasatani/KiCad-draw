4aacb6dcfc5d9612976f00b1117242750ad333d0
TSasatani <sasatani.dev@gmail.com>
2023-12-03 11:11:17 +0900

    add helix coil test for testing full flow

--- tests/test_PCBdraw.py
+++ tests/test_PCBdraw.py
@@ -1,4 +1,8 @@
 import pytest
+import filecmp
+import sys
+import os
+
 from kicad_draw.PCBmodule import PCBdraw
 
 from kicad_draw.config import default_layers
@@ -15,3 +19,47 @@ def test__can_draw_via(capsys):
     PCBdraw_ins.draw_via(x = 111.76, y = 107.315, via_size=0.8, drill_size=0.4, layer_index_1 = 0, layer_index_2 = 3, net_number=2)
     result = capsys.readouterr()
     assert result.out == '  (via (at 111.76 107.315) (size 0.8) (drill 0.4) (layers "F.Cu" "B.Cu") (net 2) (tstamp 0))\n'
+
+def test__can_draw_helix_coil(capsys):
+    dir = os.path.dirname(__file__)
+    assert_file_path = os.path.join(dir, 'assets','helix_coil_test.txt')
+    out_file_path = os.path.join(dir, 'test_output')
+    out_file_name = 'helix_coil_out.txt'
+    
+
+    print(assert_file_path)
+    print(out_file_path + '/' + out_file_name)
+
+    sys.stdout = open(out_file_path + '/' + out_file_name, 'w')
+
+    Center = [150, 100]
+    track_width = 0.5
+    connect_width = 0.2
+    radius=11
+    port_gap=0.65
+    tab_gap=0.55
+    via_size=0.4
+    drill_size=0.2
+    angle_step=0
+    Nelement=100
+    layerindexlist=[0,1,2,3,4,5]
+    netnumber=1 # find from KiCad PCB file
+
+    kicad_draw = PCBdraw(stackup='default_6layer')
+    kicad_draw.draw_helix(x0=Center[0],
+                        y0=Center[1],
+                        radius=radius,
+                        track_width=track_width,
+                        connect_width=connect_width,
+                        port_gap=port_gap,
+                        net_number=netnumber,
+                        drill_size=drill_size,
+                        via_size=via_size,
+                        tab_gap=tab_gap,
+                        angle_step=angle_step,
+                        layer_index_list=layerindexlist)
+    
+    sys.stdout.close()
+    sys.stdout = sys.__stdout__
+
+    assert filecmp.cmp(assert_file_path,out_file_path + '/' + out_file_name)
